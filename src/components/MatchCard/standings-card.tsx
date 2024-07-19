cac825c87fca36b87c2acf4430db9bd31d72ee99
Harshil Tomar <harshil.tomar@xarterian.com>
2024-07-19 12:44:10 +0530

    chore: Standings link

--- src/components/MatchCard/standings-card.tsx
+++ src/components/MatchCard/standings-card.tsx
@@ -1,7 +1,9 @@
 export default function Standings() {
   return (
     <div className='w-full p-4 rounded-b-lg bg-stone-50 dark:bg-stone-800 rounded text-left text-gray-700 dark:text-white text-base'>
-      Standings &rarr;
+      <a href="https://www.oneturf.news/" target="_blank">
+        Standings &rarr;
+      </a>
     </div>
   )
 }
